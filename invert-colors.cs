b94c1182174e9a1d510fec84d924614baf7a65e4
Orthzar <orthzar@gmail.com>
2021-03-19 20:18:58 -0500

    Update invert-colors.cs

--- invert-colors.cs
+++ invert-colors.cs
@@ -1,14 +1,17 @@
-// This program inverts the colors for a given png.
+// This program accepts a png, inverts all the colors, and outputs another file with "-inverted" added to the file.
 
 using System;
 using System.Drawing;
 
+//Bitmap image1;
+
 public class MyClass
 {
+//    public Bitmap (string filename);
     public static Image image1;
     public static void Main()
     {
-        image1 = new Bitmap("./jungle-arc.png", true); // Change to be whatever image you have
+        image1 = new Bitmap("./jungle-arc.png", true);
         
         int x, y;
         
@@ -17,13 +20,13 @@ public class MyClass
             for(y=0; y<image1.Height; y++)
             {
                 Color pixelColor = image1.GetPixel(x, y);
-                Color invRed = 2147483647 ^ Color.FromArgb(pixelColor.R);
-                Color invGreen = 2147483647 ^ Color.FromArgb(pixelColor.G); // xor'ing with an int32 of all 1's seems appropriate
-                Color invBlue = 2147483647 ^ Color.FromArgb(pixelColor.B);
-                Color invColor = Color.FromArgb(invRed, invGreen, invBlue);
-                image1.SetPixel(x, y, invColor);
+                Color invRed1 = 2147483647 ^ Color.FromArgb(pixelColor.R);
+                Color invGreen1 = 2147483647 ^ Color.FromArgb(pixelColor.G);
+                Color invBlue1 = 2147483647 ^ Color.FromArgb(pixelColor.B);
+                Color invColor1 = Color.FromArgb(invRed1, invGreen1, invBlue1);
+                image1.SetPixel(x, y, invColor1);
             }
         }
+        image1.Save("./jungle-arc-inverted.png");
     }
-    image1.Save("./jungle-arc-inverted.png");
 }
